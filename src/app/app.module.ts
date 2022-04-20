13c4c1d346dc56ac5e374253b8167cc6244d7559
Ignacio Recabarren <ignacio.recabarren@onetree.com>
2022-04-20 12:29:46 -0300

    fist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { VisualvaultLibraryModule } from 'visualvault-library';
+import { VisaulvaultlibraryModule } from 'visaulvaultlibrary';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VisualvaultLibraryModule,
+    VisaulvaultlibraryModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
